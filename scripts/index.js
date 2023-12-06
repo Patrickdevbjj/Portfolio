b03e67c0fc8bd1856e15a221da04aa278660582f
Patrick Rodrigues <patrickdev6@hotmail.com>
2023-12-06 13:30:13 -0300

    finalizando o projeto

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+const navbar = document.querySelector('.navbar');
+const mobileNavbar = document.querySelector('.navbar__mobile');
+const button = document.querySelector('.burguer');
+
+button.addEventListener('click', function () {
+  mobileNavbar.classList.toggle('active');
+});
+
+window.addEventListener('scroll', function () {
+    var navbar = document.querySelector('.navbar');
+    if (navbar) {
+      if (window.scrollY > 0) {
+        navbar.classList.add('active');
+      } else {
+        navbar.classList.remove('active');
+      }
+    }
+  });
+  
